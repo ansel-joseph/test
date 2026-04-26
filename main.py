d5f50c3a5433e268e722fb5aae16e2b4a3a7094f
Ansel Joseph <anseljoseph2509@gmail.com>
2026-04-27 01:31:15 +0530

    initialize: Text Assistant

---
+++ main.py
@@ -0,0 +1,24 @@
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
+
+client = genai.Client(api_key=api_key)
+
+print("API KEY loaded successfully.")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents = "Recite John 3:16",
+    config = types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_budget=0)
+    ),
+)
+print(response.text)
